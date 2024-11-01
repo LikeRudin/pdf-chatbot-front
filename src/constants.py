28c9d0a5d27fda43f80bf4f2614b9931e465397e
likeRudin <likeRudin@gmail.com>
2024-11-01 09:14:03 +0900

    chore: add constants for st.session_state

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class SessionStateKey:
+    LOGGED_IN: str = "logged_in"  # 로그인 여부
+    USER : str = "user" # 사용자 데이터
+    USERNAME: str = "username"  # 사용자 이름
+    TOKEN: str = "token"  # 로그인 토큰
+    CURRENT_API_KEY_ID: str = "current_key_id"  # 현재 사용중인 API key ID
+    API_KEY_LIST: str = "api_keys"  # 사용자가 등록한 key 목록
+
+SESSION_STATE_KEY = SessionStateKey()
